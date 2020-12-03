b8fbcc64b052446ab5a03909209e3bfe6022eb7d
Mike Ulch <mike@norcode.com>
2020-12-03 12:29:58 -0500

    metal's day 3 solution

--- 03/03.py
+++ 03/03.py
@@ -1,13 +1,23 @@
+# From metalhedd
 from timeit import timeit
 
 lines = open('03.txt').readlines()
-# numbers = [int(x) for x in open('03.txt').readlines()]
+lines = [list(l.strip()) for l in lines]
+
+def count_trees(dx, dy, x=0, y=0):
+    total = 0
+    while y < len(lines)-1:
+        x = (x + dx) % len(lines[0])
+        y += dy
+        if lines[y][x] == '#':
+            total += 1
+    return total
 
 def part1():
-    pass
+    print(count_trees(3, 1))
 
 def part2():
-    pass
+    print(count_trees(1, 1) * count_trees(3, 1) * count_trees(5, 1) * count_trees(7, 1) * count_trees(1, 2))
 
 if __name__ == '__main__':
     print("Part 1: ", end="")
